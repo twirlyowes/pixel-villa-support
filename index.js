708a14df3ce83892f97644f11eed83c26e32af64
twirlyowes <harshpatil200808@gmail.com>
2026-07-23 14:02:12 +0530

    Change app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -315,6 +315,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Keep-alive server is running on port ${PORT}`);
 });
