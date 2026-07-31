26b6cb394e82fe657aff5d6659f5cda2f1d3ce73
twirlyowes <harshpatil200808@gmail.com>
2026-07-31 11:55:10 +0530

    Fix indentation and formatting in voicesystem.js

--- voicesystem.js
+++ voicesystem.js
@@ -318,9 +318,9 @@ if (oldState.channelId && oldState.channelId !== newState.channelId) {
         deletionTracker.delete(channelId);
       }
 
-    }, 500);
+        }, 500);
   }
-        }
+}
   // 2. Handle Owner Customizations & Commands with Set Lookup & Early Ignoring
   client.on("messageCreate", async (message) => {
     if (message.author.bot || !message.guild) return;
